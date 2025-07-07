72528f3a194a379ace2eb17f2330b068677b0965
iSkySan <theofragale@hotmail.fr>
2025-07-07 10:41:46 +0200

    first commit

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const {createDefaultPreset} = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+    testEnvironment: "node",
+    transform: {
+        ...tsJestTransformCfg,
+    },
+    testMatch: ["<rootDir>/test/**/*.spec.ts"],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        ],
+    coverageThreshold: {
+        global: {
+            branches: 50,
+            functions: 50,
+            lines: 50,
+            statements: 50,
+        },
+    }
+};
